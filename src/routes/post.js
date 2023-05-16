c4e0ed9fb109c9ae2dd754f757802881635ffc50
Pichborith Kong <pichborith@gmail.com>
2023-05-16 16:47:26 -0700

    add deletepost function

--- src/routes/post.js
+++ src/routes/post.js
@@ -3,7 +3,7 @@ const controller = require('../controllers/post');
 
 router.post('/create', controller.addPost);
 router.get('/', controller.readAll);
-router.patch('/:postId', controller.patchPost);
-// router.delete('/:postId', controller.deletePost);
+router.patch('/:postId', controller.editPost);
+router.delete('/:postId', controller.deletePost);
 
 module.exports = router;
